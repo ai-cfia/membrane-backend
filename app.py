c0c37e8d386e269120c536c54d81da59a09c99eb
CFIALeronB <Leron.Bergelson@inspection.gc.ca>
2023-08-01 08:51:16 -0400

    Implementing email regex check
    - The endpoint, accessed via the '/validate_email' route, is designed to validate an email address provided in the request JSON data.
    -The validation process ensures that the email is both properly formatted and ends with one of the allowed domains: 'gc.ca', 'canada.ca', or 'inspection.gc.ca'.
    -If the email is valid, the function responds with a JSON object containing the message 'Valid email address.' and a status code of 200.
    -If the email is invalid or missing in the request, the function responds with a JSON object containing the error message 'Invalid email address.' and a status code of 400.

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
+"""
+CFIA Louis Backend Flask Application
+"""
+import re
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello, World!'
+@app.route('/validate_email', methods=['POST'])
+def validate_email():
+    """
+    Validates an email address received in the request JSON data.
+
+    - The function expects a JSON object with an 'email' field containing the email address 
+      to validate.
+    - It checks if the email is valid and ends with one of the allowed domains (gc.ca, canada.ca, 
+      or inspection.gc.ca).
+
+    Returns:
+        - If the email is valid, returns a JSON response with 'message': 'Valid email address.' 
+          and status code 200.
+        - If the email is invalid or missing in the request, returns a JSON response with
+          'error': 'Invalid email address.' and status code 400.
+    """
+    # Get the email address from the request JSON data
+    data = request.get_json()
+    email = data.get('email')
+
+    # Regular expression pattern to match valid email addresses
+    pattern = r'^[a-zA-Z0-9._%+-]+@(?:gc\.ca|canada\.ca|inspection\.gc\.ca)$'
+
+    if email is None:
+        return jsonify({'error': 'No email provided in the request.'}), 400
+
+    if re.match(pattern, email):
+        return jsonify({'message': 'Valid email address.'}), 200
+
+    return jsonify({'error': 'Invalid email address.'}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
