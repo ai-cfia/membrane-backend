884d1fd279cda831b6a323d2c452eb6b10a098a7
CFIALeronB <133677161+CFIALeronB@users.noreply.github.com>
2023-08-02 15:55:53 -0400

    Refactor: Implement JWT handling using flask_jwt_extended and enhance redirection flow
    - Took out the jwt library for creating and decoding JWTs and implemented the flask_jwt_extended library, which is a Flask-specific library for handling JWTs and offers some additional convenience features.
    - Adding functionality for redirecting users to a specified URL after their email has been verified. This URL is included as an additional claim in the JWT.
    - Implemented receival of the token as a URL parameter.
    - Previously the JWT is created in the create_jwt(email) function, whereas now the JWT is created directly in the login() function using the create_access_token() function from the flask_jwt_extended library. The code also includes an additional claim in the JWT for the redirect URL.

--- app.py
+++ app.py
@@ -2,95 +2,104 @@
 CFIA Louis Backend Flask Application
 """
 import logging
-import jwt
-from flask import Flask, request, jsonify, session, make_response
+from datetime import timedelta
+from jwt.exceptions import InvalidTokenError
+from flask_jwt_extended import JWTManager, create_access_token, decode_token
+from flask import Flask, request, jsonify, session, make_response, redirect, url_for
 from flask_session import Session
-from utils import is_valid_email, create_jwt
+from utils import is_valid_email
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'SECRET' # TODO: Define a secret key for the app using environment variables
-app.config['SESSION_TYPE'] = 'filesystem'  # Use file-based session storage (you can explore other storage options)
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=60)  # Set token expiry time
+jwt = JWTManager(app)
+
+# Configure Flask-Session
+app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
 @app.route('/login', methods=['POST'])
 def login():
     """
-    Validates an email address received in the request JSON data.
-
-    - The function expects a JSON object with an 'email' field containing the email address
-      to validate.
-    - It checks if the email is valid and ends with one of the allowed domains (gc.ca, canada.ca,
-      or inspection.gc.ca).
+    This endpoint handles user login. It receives an email and a redirect URL via a POST request.
+    If the email is valid and if the user is not already authenticated, a JWT token is created 
+    and a verification URL, which includes the token as a query parameter, is sent to the user's 
+    email.
+    
+    Parameters:
+    None, but expects a JSON object in the request body with 'email' and 'redirect_url' keys.
 
     Returns:
-        - If the email is valid, returns a JSON response with 'message': 'Valid email address.'
-          and status code 200.
-        - If the email is invalid or missing in the request, returns a JSON response with
-          'error': 'Invalid email address.' and status code 400.
+    A JSON response object and an HTTP status code. The response object contains a 'message' or 
+    'error' key depending on the result of the request.
+
+    The possible outcomes are:
+    - Returns a 200 HTTP status code and a message that the user is already authenticated if the 
+      user
+      is already authenticated.
+    - Returns a 200 HTTP status code and a message that the email is valid and the JWT link has 
+      been sent if
+      the email is valid and the user is not authenticated.
+    - Returns a 400 HTTP status code and an error message if the email is invalid.
+    - Returns a 400 HTTP status code and an error message if the email or the redirect URL are not 
+      provided.
     """
-    # Get the email address from the request JSON data
     data = request.get_json()
     email = data.get('email')
+    redirect_url = data.get('redirect_url')
 
-    if email is None:
-        return jsonify({'error': 'No email provided in the request.'}), 400
+    if email is None or redirect_url is None:
+        return jsonify({'error': 'Missing email or redirect URL.'}), 400
 
     if is_valid_email(email):
-        jwt_token = create_jwt(email)  # Using the create_jwt function from utils.py
-
         if 'authenticated' in session and session['authenticated']:
-            # If the user is already authenticated, return the JWT in the response body
-            return jsonify({'message': 'Valid email address.', 'jwt': jwt_token}), 200
+            return jsonify({'message': 'Already authenticated.'}), 200
 
-        # If the user is not authenticated, send a JWT link to the email
-        # (code for sending email is not implemented)
-        # SendEmailWithJWT(email)  # Implement this function to send an email with the JWT link
+        additional_claims = {"redirect_url": redirect_url}
+        access_token = create_access_token(identity=email, additional_claims=additional_claims)
+        verification_url = url_for('verify_token', token=access_token, _external=True)
 
-        # Set the 'authenticated' key in the session dictionary to indicate that the user is authenticated
-        session['authenticated'] = True
-        session['user_email'] = email
+        print(verification_url)
+        return jsonify({'message': 'Valid email address. Email sent with JWT link.'}), 200
 
-        return jsonify({'message': 'Valid email address. Email sent with JWT link.', 'jwt': jwt_token}), 200
-
+    # Note: No 'else' block here
     return jsonify({'error': 'Invalid email address.'}), 400
 
-
-
-@app.route('/verify_token/<token>', methods=['GET'])
-def verify_token(token):
+@app.route('/verify_token', methods=['GET'])
+def verify_token():
     """
     Handle the verification of the JWT token received via the link.
-
-    Parameters:
-        token (str): The JWT token extracted from the URL.
 
     Returns:
         - If the token is valid and not expired, set the session variables
           'authenticated' and 'user_email' and redirect the user to the dashboard.
         - If the token is expired or invalid, return a JSON response with an error message.
     """
+    token = request.args.get('token')  # Extract token from URL parameters
+
+    if token is None:
+        return jsonify({'error': 'No token provided.'}), 400
+
     try:
         print("Received token:", token)  # Print the received token
         # Decode and verify the JWT token using the app's secret key
-        decoded_token = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        decoded_token = decode_token(token)
         print("Decoded token:", decoded_token)  # Print the decoded token
-        email = decoded_token['email']
+        email = decoded_token['sub']
+        redirect_url = decoded_token['redirect_url']  # Extract the redirect URL from the token
 
         # Set the 'authenticated' key in the session dictionary to indicate that the user is authenticated
         session['authenticated'] = True
         session['user_email'] = email
 
-        # TODO: Redirect the user back to Louis Login page
-        return make_response('Email Verified', 200)
+        # Redirect to the extracted URL
+        response = make_response(redirect(redirect_url, code=302))
+        return response
 
-    except jwt.ExpiredSignatureError:
-        # Handle the case where the token is expired
-        return jsonify({'error': 'Token has expired.'}), 400
-    except jwt.InvalidTokenError as invalid_token_error:
+    except InvalidTokenError as invalid_token_error:
         # Log the exception message to inspect the reason for the decoding failure
         logging.error('JWT Token decoding error: %s', invalid_token_error)
         return jsonify({'error': 'Invalid token.'}), 400
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -2,9 +2,6 @@
 Utility functions
 """
 import re
-import datetime
-import jwt
-from flask import current_app
 
 def is_valid_email(email):
     """
@@ -18,24 +15,3 @@ def is_valid_email(email):
     """
     pattern = r'^[a-zA-Z0-9._%+-]+@(?:gc\.ca|canada\.ca|inspection\.gc\.ca)$'
     return bool(re.match(pattern, email))
-
-
-def create_jwt(email):
-    """
-    Create a JWT token with the provided email.
-
-    This function generates a JWT with the email as the payload and sets an expiration time.
-
-    Returns:
-        A JWT token as a string.
-    """
-    secret_key = current_app.config['SECRET_KEY']
-    
-    # Set the token expiration time (e.g., 30 minutes)
-    expiration_time = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
-    payload = {
-        'email': email,
-        'exp': expiration_time
-    }
-    jwt_token = jwt.encode(payload, secret_key, algorithm='HS256')
-    return jwt_token
