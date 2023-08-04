d58e8827a61977aaa34d006458686fca9b20af82
CFIALeronB <133677161+CFIALeronB@users.noreply.github.com>
2023-08-04 14:40:35 -0400

    Introduced dynamic fetching of allowed email domains from the environment variable
    - Introduced dynamic fetching of allowed email domains from the environment variable ALLOWED_EMAIL_DOMAINS.
    - Implemented dynamic construction of the regular expression pattern used for email validation.
    Used os.getenv to read environment variables.
    - Modified the setup of test fixtures to include environment variable loading using dotenv within individual test functions, ensuring that they have the necessary context for execution.

--- tests/api_login_test.py
+++ tests/api_login_test.py
@@ -1,19 +1,27 @@
 """
 Login API Test
 """
+import os
+from pathlib import Path  # Python 3.6+ only
 import pytest
 from flask import Flask
 from flask.testing import FlaskClient
+from dotenv import load_dotenv
 
 # Import the Flask application instance from your app module
 from app import app as flask_app
+
+env_path = Path('../') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+print(os.environ)
 
 @pytest.fixture
 def app():
     """Flask application fixture for the tests."""
     yield flask_app
     # pylint: disable=redefined-outer-name
-    
+
 @pytest.fixture
 def test_client(app: Flask):
     """Test client fixture for the tests."""
@@ -31,6 +39,7 @@ def login_url(base_url):
 
 def test_valid_login(login_url, test_client: FlaskClient):
     """Test case for valid login."""
+    os.environ['ALLOWED_EMAIL_DOMAINS'] = os.getenv('ALLOWED_EMAIL_DOMAINS')
     response = test_client.post(
         login_url,
         json={
@@ -38,10 +47,14 @@ def test_valid_login(login_url, test_client: FlaskClient):
             "redirect_url": "http://localhost:3000/"
         }
     )
+
+    allowed_domains = os.getenv('ALLOWED_EMAIL_DOMAINS', '')
+    print(allowed_domains)
     assert response.status_code == 200
 
 def test_invalid_email_format(login_url, test_client: FlaskClient):
     """Test case for invalid email format."""
+    os.environ['ALLOWED_EMAIL_DOMAINS'] = os.getenv('ALLOWED_EMAIL_DOMAINS')
     response = test_client.post(
         login_url,
         json={
@@ -53,6 +66,7 @@ def test_invalid_email_format(login_url, test_client: FlaskClient):
 
 def test_email_not_provided(login_url, test_client: FlaskClient):
     """Test case for not provided email."""
+    os.environ['ALLOWED_EMAIL_DOMAINS'] = os.getenv('ALLOWED_EMAIL_DOMAINS')
     response = test_client.post(
         login_url,
         json={
@@ -63,6 +77,7 @@ def test_email_not_provided(login_url, test_client: FlaskClient):
 
 def test_invalid_redirect_url(login_url, test_client: FlaskClient):
     """Test case for invalid redirect url."""
+    os.environ['ALLOWED_EMAIL_DOMAINS'] = os.getenv('ALLOWED_EMAIL_DOMAINS')
     response = test_client.post(
         login_url,
         json={
@@ -72,18 +87,9 @@ def test_invalid_redirect_url(login_url, test_client: FlaskClient):
     )
     assert response.status_code == 400
 
-def test_redirect_url_not_provided(login_url, test_client: FlaskClient):
-    """Test case for not provided redirect url."""
-    response = test_client.post(
-        login_url,
-        json={
-            "email": "valid.email@example.com",
-        }
-    )
-    assert response.status_code == 400
-
 def test_empty_json(login_url, test_client: FlaskClient):
     """Test case for empty json body."""
+    os.environ['ALLOWED_EMAIL_DOMAINS'] = os.getenv('ALLOWED_EMAIL_DOMAINS')
     response = test_client.post(
         login_url,
         json={}
--- utils.py
+++ utils.py
@@ -1,17 +1,20 @@
 """
 Utility functions
 """
+import os
 import re
 
 def is_valid_email(email):
     """
     Validates an email address.
 
-    It checks if the email is valid and ends with one of the allowed domains 
-    (gc.ca, canada.ca, or inspection.gc.ca).
+    It checks if the email is valid and ends with one of the allowed domains.
+    Allowed domains are fetched from the ALLOWED_EMAIL_DOMAINS environment variable.
 
     Returns:
         - True if the email is valid, False otherwise.
     """
-    pattern = r'^[a-zA-Z0-9._%+-]+@(?:gc\.ca|canada\.ca|inspection\.gc\.ca)$'
+    allowed_domains = os.getenv('ALLOWED_EMAIL_DOMAINS', '')
+    allowed_domains = '|'.join([re.escape(domain.strip()) for domain in allowed_domains.split(',')])
+    pattern = r'^[a-zA-Z0-9._%+-]+@(?:' + allowed_domains + ')$'
     return bool(re.match(pattern, email))
