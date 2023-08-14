4ecddbed49282d37312702a5ecfdd4dfbb53029d
CFIALeronB <133677161+CFIALeronB@users.noreply.github.com>
2023-08-14 10:32:55 -0400

    Updated /login test functions to support the `app_id` in JWT header
    - Updated /login test functions to support the `app_id` in JWT header to accommodate recent changes in token generation.

--- tests/api_login_test.py
+++ tests/api_login_test.py
@@ -4,17 +4,30 @@ Login API Test
 import os
 import pytest
 from flask.testing import FlaskClient
+from generate_jwt import generate_jwt
+
+TEST_PRIVATE_KEY = open('keys/test1_private_key.pem', 'rb').read()
+
+def generate_valid_jwt():
+    """Generate a valid JWT token with the required app_id"""
+    jwt_data = {
+        "app_id": "app1",
+        "redirect_url": "https://www.google.com/"
+    }
+    return generate_jwt(jwt_data, TEST_PRIVATE_KEY)
 
 @pytest.mark.usefixtures("set_allowed_domains")
 def test_valid_login(login_url, test_client: FlaskClient):
     """Test case for valid login."""
+
+    jwt_token = generate_valid_jwt()
     response = test_client.post(
-        login_url,
+        f"{login_url}?token={jwt_token}",
         json={
             "email": "valid.email@inspection.gc.ca",
-            "redirect_url": "http://localhost:3000/"
         }
     )
+
     assert response.status_code == 200
 
     allowed_domains = os.getenv('ALLOWED_EMAIL_DOMAINS', '')
@@ -24,11 +37,12 @@ def test_valid_login(login_url, test_client: FlaskClient):
 @pytest.mark.usefixtures("set_allowed_domains")
 def test_invalid_email_format(login_url, test_client: FlaskClient):
     """Test case for invalid email format."""
+
+    jwt_token = generate_valid_jwt()
     response = test_client.post(
-        login_url,
+        f"{login_url}?token={jwt_token}",
         json={
             "email": "invalid.email",
-            "redirect_url": "http://localhost:3000/"
         }
     )
     assert response.status_code == 400
@@ -36,10 +50,11 @@ def test_invalid_email_format(login_url, test_client: FlaskClient):
 @pytest.mark.usefixtures("set_allowed_domains")
 def test_email_not_provided(login_url, test_client: FlaskClient):
     """Test case for not provided email."""
+
+    jwt_token = generate_valid_jwt()
     response = test_client.post(
-        login_url,
+        f"{login_url}?token={jwt_token}",
         json={
-            "redirect_url": "http://localhost:3000/"
         }
     )
     assert response.status_code == 400
@@ -47,20 +62,12 @@ def test_email_not_provided(login_url, test_client: FlaskClient):
 @pytest.mark.usefixtures("set_allowed_domains")
 def test_invalid_redirect_url(login_url, test_client: FlaskClient):
     """Test case for invalid redirect url."""
+
+    jwt_token = generate_valid_jwt()
     response = test_client.post(
-        login_url,
+        f"{login_url}?token={jwt_token}",
         json={
             "email": "valid.email@example.com",
-            "redirect_url": "htp:/localhost"
         }
-    )
-    assert response.status_code == 400
-
-@pytest.mark.usefixtures("set_allowed_domains")
-def test_empty_json(login_url, test_client: FlaskClient):
-    """Test case for empty json body."""
-    response = test_client.post(
-        login_url,
-        json={}
     )
     assert response.status_code == 400
