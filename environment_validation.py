9e37a11658b2f9984d7e6c7a49dfc6820de688d7
K. Allagbe <kotchikpa.allagbe@gmail.com>
2023-09-27 09:42:37 -0400

    issue #42: fix lint problems

--- environment_validation.py
+++ environment_validation.py
@@ -5,7 +5,8 @@ def validate_environment_settings(
     Validate the environment settings required for the application.
 
     Args:
-    - CLIENT_PUBLIC_KEYS_DIRECTORY (Path): The directory containing the client public keys.
+    - CLIENT_PUBLIC_KEYS_DIRECTORY (Path): The directory containing the client public
+    keys.
     - SERVER_PRIVATE_KEY (Path): The path to the server's private key.
     - SERVER_PUBLIC_KEY (Path): The path to the server's public key.
     - FRONTEND_URL (str): The redirect URL to the membrane frontend.
@@ -20,25 +21,29 @@ def validate_environment_settings(
     # Check the client public keys directory
     if not CLIENT_PUBLIC_KEYS_DIRECTORY.exists():
         raise ValueError(
-            f"The directory {CLIENT_PUBLIC_KEYS_DIRECTORY} for public keys does not exist. Please specify a valid directory."
+            f"The directory {CLIENT_PUBLIC_KEYS_DIRECTORY} for public keys does not "
+            "exist. Please specify a valid directory."
         )
 
     # Check the server private key
     if not SERVER_PRIVATE_KEY.exists() or SERVER_PRIVATE_KEY.is_dir():
         raise ValueError(
-            f"The specified server private key file {SERVER_PRIVATE_KEY} does not exist or is a directory. Please provide a valid path."
+            f"The specified server private key file {SERVER_PRIVATE_KEY} does not "
+            "exist or is a directory. Please provide a valid path."
         )
 
     # Check the server public key
     if not SERVER_PUBLIC_KEY.exists() or SERVER_PUBLIC_KEY.is_dir():
         raise ValueError(
-            f"The specified server public key file {SERVER_PUBLIC_KEY} does not exist or is a directory. Please provide a valid path."
+            f"The specified server public key file {SERVER_PUBLIC_KEY} does not exist "
+            "or is a directory. Please provide a valid path."
         )
 
     # Check the redirect URL to Membrane Frontend
     if not FRONTEND_URL:
         raise ValueError(
-            "The redirect URL to Membrane Frontend is not specified. Please provide a valid URL."
+            "The redirect URL to Membrane Frontend is not specified. Please provide a "
+            "valid URL."
         )
 
     return True
