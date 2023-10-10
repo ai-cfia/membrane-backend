3977e1f9c969a1c1f0b109a0c2c9b2fff9da951c
k-allagbe <kotchikpa.allagbe@gmail.com>
2023-10-10 20:43:25 +0000

    issue #75: implement optional vs required

--- dynamic_env_validator.py
+++ dynamic_env_validator.py
@@ -5,16 +5,25 @@ from pydantic import EmailStr, Field, create_model
 from pydantic_settings import BaseSettings
 
 
-class FileStr(str):
+class PathStr(str):
     @classmethod
     def __get_validators__(cls):
         yield cls.validate
 
     @classmethod
     def validate(cls, value, field):
-        if not os.path.exists(value):
-            raise ValueError(f"File {value} doesn't exist.")
+        validator = getattr(cls, "_validator", None)
+        if validator is None or not validator(value):
+            raise ValueError(f"{value} is not a valid {cls.__name__} or doesn't exist.")
         return value
+
+
+class FileStr(PathStr):
+    _validator = staticmethod(os.path.isfile)
+
+
+class DirStr(PathStr):
+    _validator = staticmethod(os.path.isdir)
 
 
 TYPE_MAPPING = {
@@ -23,7 +32,8 @@ TYPE_MAPPING = {
     "float": float,
     "bool": bool,
     "EmailStr": EmailStr,
-    "FileStr": FileStr,  # Add FileStr here
+    "FileStr": FileStr,
+    "DirStr": DirStr,
 }
 
 
@@ -37,12 +47,20 @@ def create_dynamic_settings(yaml_filename: str) -> BaseSettings:
     fields = {}
     for name, value in config.items():
         field_info = value.get("field_info", {})
-        field_type = TYPE_MAPPING.get(value.get("type"))
-        fields[name] = (field_type, Field(**field_info))
+        field_type_str = value.get("type")
+        field_type = TYPE_MAPPING.get(field_type_str)
+        
+        is_optional = value.get("optional", False)
+        if is_optional:
+            field_type = field_type | None
+            field_info["default"] = field_info.get("default", None)
 
+        fields[name] = (field_type, Field(**field_info))
     return create_model("DynamicSettings", __base__=BaseSettings, **fields)
 
 
 DynamicSettings = create_dynamic_settings("env.yaml")
 settings = DynamicSettings(_env_file=".env")
 print(settings.dict())
+print(settings.MEMBRANE_FRONTEND)
+print(DynamicSettings.model_fields["MEMBRANE_FRONTEND"].description)
