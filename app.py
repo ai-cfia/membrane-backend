303d8f27e8cec8f6b809de2e79651cb39b3fbe82
K. Allagbe <kotchikpa.allagbe@gmail.com>
2023-10-22 13:59:31 -0400

    issue #35: load client keys

--- app.py
+++ app.py
@@ -3,15 +3,9 @@ CFIA Membrane Backend Flask Application
 """
 from app_creator import create_app
 from config import AppConfig
-from environment_validation import validate_environment_settings
 
 config = AppConfig()
-validate_environment_settings(
-    config.JWT_CONFIG.client_public_keys_folder,
-    config.JWT_CONFIG.server_private_key,
-    config.JWT_CONFIG.server_public_key,
-    config.MEMBRANE_FRONTEND,
-)
+# TODO: env var validation #75
 app = create_app(config)
 
 if __name__ == "__main__":
